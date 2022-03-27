9adc4f0f0d61ce4012a3bc91b3f87ee6a062abbf
DeliaDumitrescu <deliadum123@gmail.com>
2022-03-27 12:32:33 +0300

    add nav

--- app/src/main/java/com/example/kotlinapp/TakePhoto.kt
+++ app/src/main/java/com/example/kotlinapp/TakePhoto.kt
@@ -4,8 +4,27 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.app.Activity
+import android.graphics.Bitmap
+//import androidx.appcompat.app.AppCompatActivity
+//import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 
 class TakePhoto : AppCompatActivity() {
+    private lateinit var resultLauncher: ActivityResultLauncher<Intent>
+    private lateinit var btnOpenCamera: Button
+    private lateinit var ivPhoto: ImageView
+
+    private fun handleCameraImage(intent: Intent?) {
+        val bitmap = intent?.extras?.get("data") as Bitmap
+        ivPhoto.setImageBitmap(bitmap)
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_take_photo)
@@ -28,5 +47,21 @@ class TakePhoto : AppCompatActivity() {
             }
             false
         })
+
+        btnOpenCamera = findViewById(R.id.button)
+        ivPhoto = findViewById(R.id.imageView)
+
+        resultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    handleCameraImage(result.data)
+                }
+            }
+
+        btnOpenCamera.setOnClickListener {
+            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            resultLauncher.launch(cameraIntent)
+
+        }
     }
 }
