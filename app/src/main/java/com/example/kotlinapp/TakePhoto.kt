173f8032d387b1fd295b25d34691216cd4d26016
DeliaDumitrescu <deliadum123@gmail.com>
2022-03-27 12:18:41 +0300

    add nav

--- app/src/main/java/com/example/kotlinapp/TakePhoto.kt
+++ app/src/main/java/com/example/kotlinapp/TakePhoto.kt
@@ -1,30 +1,30 @@
 package com.example.kotlinapp
+
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-
-class MainActivity : AppCompatActivity() {
+class TakePhoto : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_take_photo)
 
         // Initialize and assign variable
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
 
         // Set Home selected
-        bottomNavigationView.selectedItemId = R.id.home
+        bottomNavigationView.selectedItemId = R.id.photo
 
         // Perform item selected listener
         bottomNavigationView.setOnNavigationItemSelectedListener(BottomNavigationView.OnNavigationItemSelectedListener { item ->
             when (item.itemId) {
-                R.id.home -> true
-                R.id.photo -> {
-                    startActivity(Intent(applicationContext, TakePhoto::class.java))
+                R.id.home -> {
+                    startActivity(Intent(applicationContext, MainActivity::class.java))
                     overridePendingTransition(0, 0)
                     return@OnNavigationItemSelectedListener true
                 }
+                R.id.photo -> return@OnNavigationItemSelectedListener true
             }
             false
         })
